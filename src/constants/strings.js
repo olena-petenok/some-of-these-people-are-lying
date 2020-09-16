fb699ea8805c7011314b7fd7ecd7821a75e3a1fe
Olena Petenok <olena.petenok.work@gmail.com>
2020-09-16 22:01:56 +0300

    project rebuild

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -1,55 +1,54 @@
 // hostName
-export const HOSTNAME_OLEG = "infiny.ddns.net:50002";
-export const HOSTNAME = "212.201.71.26:50002";
+export const HOSTNAME = '';
 
 // button
-export const BUTTON_OK = "Ok";
-export const BUTTON_CANCEL = "Cancel";
-export const BUTTON_CONNECT = "Connect";
-export const BUTTON_DISCONNECT = "Disconnect";
-export const BUTTON_START_GAME = "Start Game";
-export const BUTTON_END_GAME = "End Game";
-export const BUTTON_SEND_AMOUNT_OF_PLAYERS = "Send amount of players";
-export const BUTTON_SEND_SELECTED_PLAYERS = "Send selected players";
+export const BUTTON_OK = 'Ok';
+export const BUTTON_CANCEL = 'Cancel';
+export const BUTTON_CONNECT = 'Connect';
+export const BUTTON_DISCONNECT = 'Disconnect';
+export const BUTTON_START_GAME = 'Start Game';
+export const BUTTON_END_GAME = 'End Game';
+export const BUTTON_SEND_AMOUNT_OF_PLAYERS = 'Send amount of players';
+export const BUTTON_SEND_SELECTED_PLAYERS = 'Send selected players';
 
 // input
-export const INPUT_DEFAULT = "Input something";
-export const INPUT_INVALID = "Invalid input";
-export const INPUT_HOSTNAME = "Enter hostname";
+export const INPUT_DEFAULT = 'Input something';
+export const INPUT_INVALID = 'Invalid input';
+export const INPUT_HOSTNAME = 'Enter hostname';
 export const INPUT_NICKNAME = "Enter user's nickname";
-export const INPUT_TOPIC = "Enter new topic";
-export const INPUT_AMOUNT_OF_PLAYERS = "Enter the amount of players";
+export const INPUT_TOPIC = 'Enter new topic';
+export const INPUT_AMOUNT_OF_PLAYERS = 'Enter the amount of players';
 
 // action
-export const ACTION = "command";
-export const ACTION_INTRODUCTION = "introduction";
-export const ACTION_TOPIC = "topic";
-export const ACTION_GAME_MASTER = "GMcommand";
-export const ACTION_START_GAME = "startRound";
-export const ACTION_END_GAME = "endRound";
-export const ACTION_CHANGE_USER_SCORE = "changePoints";
+export const ACTION = 'command';
+export const ACTION_INTRODUCTION = 'introduction';
+export const ACTION_TOPIC = 'topic';
+export const ACTION_GAME_MASTER = 'GMcommand';
+export const ACTION_START_GAME = 'startRound';
+export const ACTION_END_GAME = 'endRound';
+export const ACTION_CHANGE_USER_SCORE = 'changePoints';
 
 // action dataName
-export const ACTION_DATANAME_NAME = "name";
-export const ACTION_DATANAME_TOPIC = "topic";
-export const ACTION_DATANAME_GAME_MASTER = "GMaction";
-export const ACTION_DATANAME_PLAYERS = "guessers";
-export const ACTION_DATANAME_SCORE = "points";
+export const ACTION_DATANAME_NAME = 'name';
+export const ACTION_DATANAME_TOPIC = 'topic';
+export const ACTION_DATANAME_GAME_MASTER = 'GMaction';
+export const ACTION_DATANAME_PLAYERS = 'guessers';
+export const ACTION_DATANAME_SCORE = 'points';
 
 // action received
-export const ACTION_RECEIVED_USERDATA = "playerNamed";
-export const ACTION_RECEIVED_PLAYERS = "players";
-export const ACTION_RECEIVED_TOPICS = "topics";
-export const ACTION_RECEIVED_START_GAME = "startRound";
-export const ACTION_RECEIVED_END_GAME = "endRound";
-export const ACTION_RECEIVED_SERVER_ERROR = "error";
+export const ACTION_RECEIVED_USERDATA = 'playerNamed';
+export const ACTION_RECEIVED_PLAYERS = 'players';
+export const ACTION_RECEIVED_TOPICS = 'topics';
+export const ACTION_RECEIVED_START_GAME = 'startRound';
+export const ACTION_RECEIVED_END_GAME = 'endRound';
+export const ACTION_RECEIVED_SERVER_ERROR = 'error';
 
 // caption
-export const CAPTION_PLAYERS = "Players";
-export const CAPTION_ONLINE_PLAYERS = "Available players";
-export const CAPTION_YOUR_TOPICS = "Your topics";
-export const CAPTION_THE_GAME_IS_ON = "The Game is on :)";
+export const CAPTION_PLAYERS = 'Players';
+export const CAPTION_ONLINE_PLAYERS = 'Available players';
+export const CAPTION_YOUR_TOPICS = 'Your topics';
+export const CAPTION_THE_GAME_IS_ON = 'The Game is on :)';
 
 // error
-export const ERROR_EMPTY_LIST = "The list is empty";
-export const ERROR_NO_TOPIC = "The topic is absent";
+export const ERROR_EMPTY_LIST = 'The list is empty';
+export const ERROR_NO_TOPIC = 'The topic is absent';
